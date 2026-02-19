7fc58b7e2723e347201897ea1bb5c24e054773b8
Ayush-2006-py <ayushmandaokar407@gmail.com>
2026-02-19 21:06:27 +0530

    server.py

--- server.py
+++ server.py
@@ -2,11 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from process_incoming import handle_query
 from fastapi.middleware.cors import CORSMiddleware
-from flask import Flask, request, jsonify
-from flask_cors import CORS  # <--- Ye add karo
 
-app = Flask(__name__)
-CORS(app)  # <--- Ye line saari requests allow kar degi
 
 
 app = FastAPI()
