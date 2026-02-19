64314d686756d4e003fbebd3fe986c35c00e82af
Ayush-2006-py <ayushmandaokar407@gmail.com>
2026-02-19 20:35:03 +0530

    Added CORS middleware

--- server.py
+++ server.py
@@ -1,9 +1,22 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from process_incoming import handle_query
+from fastapi.middleware.cors import CORSMiddleware
+from flask import Flask, request, jsonify
+from flask_cors import CORS  # <--- Ye add karo
+
+app = Flask(__name__)
+CORS(app)  # <--- Ye line saari requests allow kar degi
+
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 class Question(BaseModel):
     question: str
 
